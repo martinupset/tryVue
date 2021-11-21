7fd6fccdf518f5bb078020cac51c30c62a007266
Martinupset <1023090715@qq.com>
2021-11-22 01:26:20 +0800

    FE post function

--- vuedemo/src/components/MyTable.vue
+++ vuedemo/src/components/MyTable.vue
@@ -1,14 +1,51 @@
 <template>
   <div class="">
+    <el-header style="text-align: right; font-size: 12px">
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-popover
+            placement="right"
+            width="400"
+            trigger="click">
+            <el-input
+              size="mini"
+              placeholder="商品名称"
+              v-model="name">
+            </el-input>
+            <el-input
+              size="mini"
+              placeholder="商品描述"
+              v-model="description">
+            </el-input>
+            <el-input
+              size="mini"
+              placeholder="商品价格"
+              v-model="price">
+            </el-input>
+            <el-input
+              size="mini"
+              placeholder="库存"
+              v-model="stock">
+            </el-input>
+            <el-button type="primary" round size="mini" @click.native="handleSubmit">确定录入</el-button>
+            <el-button slot="reference">录入商品</el-button>
+          </el-popover>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </el-header>
     <el-table :data="tableData">
-      <el-table-column prop="date" label="日期" width="140"> </el-table-column>
-      <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
-      <el-table-column prop="address" label="地址"> </el-table-column>
+      <el-table-column prop="name" label="商品名称" width="140"> </el-table-column>
+      <el-table-column prop="description" label="商品描述" width="120"> </el-table-column>
+      <el-table-column prop="price" label="商品价格"> </el-table-column>
+      <el-table-column prop="stock" label="库存"> </el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+  const baseUrl = 'http://127.0.0.1:3333/products'
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
 
@@ -16,25 +53,50 @@ export default {
   //import引入的组件需要注入到对象中才能使用
   components: {},
   data() {
-      const item = {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      };
       return {
-        tableData: Array(20).fill(item)
-      }   
+        tableData: [],
+        name: '',
+        description: '',
+        price: '',
+        stock: '',
+      }
   },
   //监听属性 类似于data概念
   computed: {},
   //监控data中的数据变化
   watch: {},
   //方法集合
-  methods: {},
+  methods: {
+    async handleSubmit() {
+      try {
+        const productData = {
+          'name': this.name,
+          'description': this.description,
+          'price': this.price,
+          'stock': this.stock
+        }
+        const addProductItem = await axios.post(baseUrl, productData)
+        this.tableData.push(productData)
+        this.name = ''
+        this.description = ''
+        this.price = ''
+        this.stock = ''
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {},
   //生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {},
+  async mounted() {
+    try {
+      const myData = await axios.get(baseUrl)
+      this.tableData = myData.data
+    } catch(e) {
+      console.log(e)
+    }
+  },
   beforeCreate() {}, //生命周期 - 创建之前
   beforeMount() {}, //生命周期 - 挂载之前
   beforeUpdate() {}, //生命周期 - 更新之前
